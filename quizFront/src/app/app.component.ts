ab50ef8db91b3e27433a13e033014bd068b7b57a
Jorge_Sagrero <jsagreroperez@gmail.com>
2018-09-23 13:51:31 -0400

    cleaned

--- quizFront/src/app/app.component.ts
+++ quizFront/src/app/app.component.ts
@@ -9,13 +9,18 @@ import { Router, NavigationStart } from '@angular/router';
 export class AppComponent {
   title = 'quizFront';
   router: boolean;
-
+  userHome: boolean;
+  navigate: boolean;
   checks:boolean;
   
   constructor(private _router: Router) {
     _router.events.forEach((event) => {
       if(event instanceof NavigationStart) {
         this.router = event.url !== "/";
+        //if current page is not app-root, hide app-root
+        // this.userHome = (event.url !== "/") &&(event.url !== "/default") &&(event.url !== "/login") &&(event.url !== "/createUser")
+        // //if current page is not within logged in, hide dashboard
+        // this.navigate = (event.url === "/default") ||(event.url === "/login") ||(event.url === "/createUser")
       }
     });
     
