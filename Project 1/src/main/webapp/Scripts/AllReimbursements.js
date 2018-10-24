0da632a2d40e8d236818a585c89374f14de6f03f
jsagreroperez <33699512+jsagreroperez@users.noreply.github.com>
2018-10-24 09:36:11 -0400

    Update AllReimbursements.js

--- Project 1/src/main/webapp/Scripts/AllReimbursements.js
+++ Project 1/src/main/webapp/Scripts/AllReimbursements.js
@@ -14,23 +14,7 @@ function sendAjaxGet(url, func){
 	xhr.open("GET", url);
 	xhr.send();
 }
-//"email": "JSAGREROPEREZ@GMAIL.COM",
-//"pswrd": "PASSWORD123",
-//"firstName": "JORGE",
-//"lastName": "SAGRERO",
-//"phoneNumber": 6614964009,
-//"address": "1530 Cherry Street,Baking CA 93305",
-//"manager": 0,
-//"loggedIn": "FALSE",
-//"accountNumber": [
-//{
-//"receipt": 1,
-//"amount": 500,
-//"reason": "BITCH YOU OWE ME",
-//"status": 0,
-//"managerId": 0,
-//"accountNumber": 1
-//},
+
 
 sendAjaxGet(privateReceiptsUrl, populateUser);
 
